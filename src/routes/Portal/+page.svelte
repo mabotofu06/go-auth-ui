d8f5e7f53141d334f7c331a7c18178a0409facab
mabotofu06 <kaki.tofu06@gmail.com>
2024-09-15 18:08:01 +0900

    静的ファイル生成のため調整中

---
+++ src/routes/Portal/+page.svelte
@@ -0,0 +1,49 @@
+<script lang="ts">
+  const iconClass = "w-96 h-96 bg-gray-300";
+</script>
+
+<div class="flex flex-col items-center py-5">
+  <h1 class="text-gray-500 text-3xl mb-10">
+    ポータルサイト
+  </h1>
+
+  <div class="mb-8">
+    タイルマップ
+    <div class="flex justify-center mb-3">
+      <div class="w-[200px] h-[120px] bg-gray-200 mx-1">
+        test
+      </div>
+      <div class="w-[200px] h-[120px] bg-gray-200 mx-1">
+        test
+      </div>
+      <div class="w-[200px] h-[120px] bg-gray-200 mx-1">
+        test
+      </div>
+    </div>
+
+    <div class="flex justify-center mb-3">
+      <div class="w-[200px] h-[120px] bg-gray-200 mx-1">
+        test
+      </div>
+      <div class="w-[200px] h-[120px] bg-gray-200 mx-1">
+        test
+      </div>
+      <div class="w-[200px] h-[120px] bg-gray-200 mx-1">
+        test
+      </div>
+    </div>
+  </div>
+
+  <div class="mb-8 w-[600px]">
+    カラムマップ
+    <div class="h-[120px] bg-gray-200 mb-3">
+      test
+    </div>
+    <div class="h-[120px] bg-gray-200 mb-3">
+      test
+    </div>
+    <div class="h-[120px] bg-gray-200 mb-3">
+      test
+    </div>
+  </div>
+</div>
